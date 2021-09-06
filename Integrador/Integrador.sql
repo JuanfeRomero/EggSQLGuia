23118d787a665c1da8ceeed2957ad52b380f2e55
JuanfeRomero <juanferomerocode@gmail.com>
2021-09-06 20:41:16 -0300

    terminado

--- Integrador/Integrador.sql
+++ Integrador/Integrador.sql
@@ -28,14 +28,39 @@ WHERE temporada LIKE "%99%";
 # CANDADO C
 # Posicion C: La posición del código será igual a la cantidad de jugadores que proceden de Michigan y forman parte de equipos de la conferencia oeste.
 # Al resultado obtenido lo dividiremos por la cantidad de jugadores cuyo peso es mayor o igual a 195, y a eso le vamos a sumar 0.9945;
-SELECT COUNT(codigo) FROM jugadores j
-JOIN equipos e ON j.Nombre_equipo = e.Nombre
-WHERE procedencia LIKE "%Michigan%" AND e.conferencia LIKE "West";
+SELECT 
+	(SELECT COUNT(codigo) FROM jugadores j
+	JOIN equipos e ON j.Nombre_equipo = e.Nombre
+	WHERE procedencia LIKE "Michigan" AND e.conferencia LIKE "West")
+/ 
+	(SELECT COUNT(codigo) FROM jugadores
+	WHERE peso >= 195) + 0.9945;
+# 1
 
 # Clave C: Para obtener el siguiente codigo deberas redondear hacia abajo el resultado que se devuelve de sumar: 
 # el promedio de puntos por partido, el conteo de asistencias por partido y la suma de tapones por partido. 
 # Ademas este resultado debe ser, donde la division central.
+SELECT FLOOR(AVG(puntos_por_partido)+ COUNT(Asistencias_por_partido) + SUM(tapones_por_partido)) FROM estadisticas e
+JOIN jugadores j ON e.jugador = j.codigo
+JOIN equipos eq ON j.nombre_equipo = eq.nombre
+WHERE eq.division LIKE "Central";
+# 631
 
+# CANDADO D
+# Posicion D: Muestre los tapones por partido del jugador Corey Maggette durante la temporada 00/01.
+# Este resultado debe ser redondeado. 
+SELECT ROUND(tapones_por_partido) FROM estadisticas
+WHERE jugador = (SELECT codigo FROM jugadores WHERE nombre LIKE "Corey Maggette") AND temporada LIKE "00/01";
+# 4
+
+# Clave D: redondear hacia abajo la suma de puntos por partido de todos los jugadores de procendencia argentina.
+SELECT FLOOR(SUM(puntos_por_partido)) FROM estadisticas e
+JOIN jugadores j ON e.jugador = j.codigo
+WHERE j.procedencia LIKE "Argentina";
+# 191
+
+
+# 1:C 631 #2:A 14043 #3:B 3480 #4:D 191
 
 SELECT * FROM equipos;
 SELECT * FROM estadisticas ORDER BY Asistencias_por_partido DESC;
